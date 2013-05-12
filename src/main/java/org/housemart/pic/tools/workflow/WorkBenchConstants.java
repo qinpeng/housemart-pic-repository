d3314e554a05b1e74af2b3e942cd2a40436f0ba8
qinpeng <qinpeng82921@163.com>
2013-05-12 20:10:36 +0800

    update

--- src/main/java/org/housemart/pic/tools/workflow/WorkBenchConstants.java
+++ src/main/java/org/housemart/pic/tools/workflow/WorkBenchConstants.java
@@ -5,7 +5,7 @@
 package org.housemart.pic.tools.workflow;
 
 public interface WorkBenchConstants {
-	static String FILE_SUBPATH_RESIDENCE = "/residence/";
-	static String FILE_SUBPATH_HOUSE = "/house/";
-	static String FILE_SUBPATH_ROOM = "/room/";
+	static String FILE_SUBPATH_RESIDENCE = "/residence";
+	static String FILE_SUBPATH_HOUSE = "/house";
+	static String FILE_SUBPATH_ROOM = "/room";
 }
