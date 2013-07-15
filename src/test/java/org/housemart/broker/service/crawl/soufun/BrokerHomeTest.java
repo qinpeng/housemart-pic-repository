1aba6f39fd93b8bb34715f6103181141cb532036
qinpeng <qinpeng82921@163.com>
2013-07-15 21:33:06 +0800

    u

--- src/test/java/org/housemart/broker/service/crawl/soufun/BrokerHomeTest.java
+++ src/test/java/org/housemart/broker/service/crawl/soufun/BrokerHomeTest.java
@@ -18,24 +18,27 @@ public class BrokerHomeTest {
   
   @Test
   public void crawlByRegion() throws Exception {
-    // brokerHome.crawl("a025");
-    // brokerHome.crawl("a020");
-    // brokerHome.crawl("a021");
-    // brokerHome.crawl("a019");
-    // brokerHome.crawl("a024");
+//    brokerHome.crawl("a025");//浦东
+//    brokerHome.crawl("a020");//长宁
+//    brokerHome.crawl("a021");//静安
+//    brokerHome.crawl("a019");//徐汇
+//    brokerHome.crawl("a024");//黄浦
+//    
+//    brokerHome.crawl("a018");//闵行
+//    brokerHome.crawl("a0586");//松江
+//    
+//    brokerHome.crawl("a030"); // 宝山
+//    brokerHome.crawl("a0996"); // 崇明
+//    brokerHome.crawl("a032");// 奉贤
+//    brokerHome.crawl("a023");// 虹口
     
-    // brokerHome.crawl("a018");
-    // brokerHome.crawl("a0586");
-    
-    brokerHome.crawl("a030");
-    brokerHome.crawl("a0996");
-    brokerHome.crawl("a032");
-    brokerHome.crawl("a023");
-    
-    
-    
-    
-    
+    brokerHome.crawl("a029");// 嘉定
+    brokerHome.crawl("a035");// 金山
+    brokerHome.crawl("a022");// 卢湾
+    brokerHome.crawl("a028");// 普陀
+    brokerHome.crawl("a031");// 青浦
+    brokerHome.crawl("a026");// 杨浦
+    brokerHome.crawl("a027");// 闸北
     
   }
 }
